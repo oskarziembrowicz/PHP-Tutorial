2d2eddb8fd44cface53a32d004f9a23ec55211b4
oskarziembrowicz <rakson2002@gmail.com>
2024-12-14 17:46:47 +0100

    Selecting rows from a database

---
+++ learning-files/sqli-select.php
@@ -0,0 +1,18 @@
+<?php
+    include("database.php");
+
+    $sql = "SELECT * FROM users";
+    $result = mysqli_query($conn, $sql);
+
+    if(mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)) {
+            echo $row["id"] . "<br>";
+            echo $row["user"] . "<br>";
+            echo $row["reg_date"] . "<br>";
+        }
+    } else {
+        echo "No user found <br>";
+    }
+
+    mysqli_close($conn);
+?>
